85fe8979a6445313b2b86919f1c5009b94e67aa7
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-08-31 01:36:54 -0300

    subiendo pagos1

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import Payment from '../models/PaymentModel.js'
-import { createPayment, updatePayment } from '../controllers/paymentController.js'
+import { createPayment } from '../controllers/paymentController.js'
 
 export const paymentRoutes = express.Router()
 
@@ -12,6 +12,5 @@ paymentRoutes.get('/', async (req, res) => {
 
 paymentRoutes.post("/", createPayment)
 
-paymentRoutes.put('/:id', updatePayment)
 
 
