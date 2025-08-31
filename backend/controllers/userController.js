a6f44372c6fceee4eb09a620dcadaa8f65a9e776
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-08-31 01:40:38 -0300

    subiendo pagos1

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,17 +36,3 @@ export const createUser = async (req, res) => {
     }
 }
 
-
-
-export const getUserWithPayments = async (req, res) => {
-    try {
-        const { name } = req.params
-        const user = await User.findOne({ name }).populate('payments')
-        if (!user) return res.status(404).json({ message: 'Usuario no encontrado' })
-
-        res.status(200).json(user)
-    } catch (error) {
-        console.error('Error al obtener usuario:', error)
-        res.status(500).json({ message: 'Error al obtener usuario' })
-    }
-}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import { createUser, getUserWithPayments } from '../controllers/userController.js'
+import { createUser } from '../controllers/userController.js'
 
 export const userRoutes = express.Router()
 
 userRoutes.post("/", createUser)
 
-userRoutes.get('/:name', getUserWithPayments)
 
 
 
